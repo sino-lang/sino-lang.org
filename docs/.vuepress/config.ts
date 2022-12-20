a26d9fb8aab7425d29c48995eec2b832445ef634
sinodev <sino@sinoarmy.com>
2022-12-20 14:50:31 +0800

    test

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,5 +1,14 @@
-import { defineUserConfig } from 'vuepress'
+import { defineUserConfig, defaultTheme  } from 'vuepress'
 
 export default defineUserConfig({
   // base:'/sino-lang.org/',
+  theme: defaultTheme({
+    // default theme config
+    navbar: [
+      {
+        text: 'Home',
+        link: '/',
+      },
+    ],
+  }),
 })
